b84a15fda74a799e3ae93ad3bb30156b818c714f
Slaykha <kadirengin2000@hotmail.com>
2023-02-02 23:24:23 +0300

    initial commit.

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        //Check if inside the bound
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            //left
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+
+            }
+            //right
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        //Check if inside the bound
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            //top
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+
+            }
+            //down
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+
+    }
+
+}
