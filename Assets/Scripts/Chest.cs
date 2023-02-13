710e486e125ae3acb279d62775e6a08679d437a2
Slaykha <kadirengin2000@hotmail.com>
2023-02-13 18:48:43 +0300

    add Collectable file, update Chest inherits from Collectable.

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chest : Collidable
+public class Chest : Collectable
 {
     protected override void OnCollide(Collider2D col)
     {
---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : Collidable
+{
+    //Logic
+    protected bool collected;
+   
+}
